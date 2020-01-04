441396ce8f805598df9b877aede72edb0f4d9aa8
James <james@smartrhinolabs.com>
2020-01-04 15:35:56 -0700

    Fixing exporting

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import ExpressionLanguage from "./ExpressionLanguage";
+import {tokenize} from "./Lexer";
+import Parser from "./Parser";
+import ExpressionFunction from "./ExpressionFunction";
+import Compiler from "./Compiler";
+import ArrayAdapter from "./Cache/ArrayAdapter";
+import StringProvider from "./Provider/StringProvider";
+import ArrayProvider from "./Provider/ArrayProvider";
+
+export default ExpressionLanguage;
+
+export {
+    Parser,
+    tokenize,
+    ExpressionFunction,
+    Compiler,
+    ArrayAdapter,
+    StringProvider,
+    ArrayProvider
+}
